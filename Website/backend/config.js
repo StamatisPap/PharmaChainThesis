be80c90a7ebfc1dc02e95dccd9e88a1d3e02b533
StamatisPap <stampap1997@gmail.com>
2022-12-12 22:47:27 +0200

    Update config.js

--- Website/backend/config.js
+++ Website/backend/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   secret: "newCodeVerify",
-  CONNECTION_URL: "mongodb+srv://StamPap97:15965ffcsloO21@cluster0.qqdc3gs.mongodb.net/?retryWrites=true&w=majority",
+  CONNECTION_URL: ""
 };
